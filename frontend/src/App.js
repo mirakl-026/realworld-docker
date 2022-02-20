f7a8f0048106036397fb79dd5d0dbd9cf18b601c
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-20 14:23:22 +0300

    front +

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload 231.
+          Edit <code>src/App.js</code> and save to reload 232.
         </p>
         <a
           className="App-link"
