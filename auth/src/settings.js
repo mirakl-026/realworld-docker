057206c20c520748718cd475f8ff31ad1b55e66c
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2022-02-20 14:13:48 +0300

    front service

--- auth/src/settings.js
+++ auth/src/settings.js
@@ -1,3 +1,3 @@
-module.exports.port = process.env.PORT || 3001;
+module.exports.port = process.env.PORT || 3002;
 module.exports.host = process.env.HOST || "http://realworld-docker.com";
 module.exports.db = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/rwd_auth";
